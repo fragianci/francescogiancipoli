b0098cd2e274f67418d35e5e3331d55a722c97ec
Francesco <francesco.giancipoli@glue-labs.com>
2023-06-02 10:26:39 +0200

    fix img, fix age

--- Js/script.js
+++ Js/script.js
@@ -1,43 +1,39 @@
-(function() {
-    // https://dashboard.emailjs.com/admin/integration
-    emailjs.init('user_NIHnHXYmsCWOlgEzdqfmD');
+(function () {
+  // https://dashboard.emailjs.com/admin/integration
+  emailjs.init('user_NIHnHXYmsCWOlgEzdqfmD');
 })();
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-            event.preventDefault();
-            // generate a five digit number for the contact_number variable
-            this.contact_number.value = Math.random() * 100000 | 0;
-            // Service ID for gmail and Template ID
-            emailjs.sendForm('service_32y0agp', 'template_6mem2gj', this)
-                .then(function() {
-                    console.log('SUCCESS!');
-                    alert('Message sent!');
-                }, function(error) {
-                    console.log('FAILED...', error);
-                    alert('Error..');
-                });
-        });
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // generate a five digit number for the contact_number variable
+    this.contact_number.value = (Math.random() * 100000) | 0;
+    // Service ID for gmail and Template ID
+    emailjs.sendForm('service_32y0agp', 'template_6mem2gj', this).then(
+      function () {
+        console.log('SUCCESS!');
+        alert('Message sent!');
+      },
+      function (error) {
+        console.log('FAILED...', error);
+        alert('Error..');
+      }
+    );
+  });
+};
+
+$(() => {
+  var media = window.matchMedia('(max-width: 960px)');
+  let makeMedia = (media) => {
+    if (media.matches) {
+      $('.navbar-toggler').click(() => {
+        $('#collapsibleNavbar').slideToggle();
+      });
+      $('.nav-link').click(() => {
+        $('#collapsibleNavbar').slideToggle();
+        $('#collapsibleNavbar').css('display', 'none');
+      });
     }
-
-
-$(()=>{
-
-    var media = window.matchMedia('(max-width: 960px)');
-    let makeMedia=media=>{
-        if(media.matches){
-            $('.navbar-toggler').click(()=>{
-                $('#collapsibleNavbar').slideToggle();
-            });
-            $('.nav-link').click(()=>{
-                $('#collapsibleNavbar').slideToggle();
-                $('#collapsibleNavbar').css('display', 'none');
-            });
-        }
-    }
-    makeMedia(media);
-    media.addListener(makeMedia);
-    
+  };
+  makeMedia(media);
+  media.addListener(makeMedia);
 });
-
-
-    
